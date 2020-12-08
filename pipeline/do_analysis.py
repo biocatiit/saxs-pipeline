561b2d62f4cc233f2dbefe6cf7db035c9bbea9c4
jbhopkins <hopkins.jesse@gmail.com>
2020-12-08 16:50:04 -0600

    Changed the name of reduce.py to reduce_data.py so it doesn't conflict with a python built in function when imported.

--- pipeline/do_analysis.py
+++ pipeline/do_analysis.py
@@ -41,3 +41,29 @@ def analyze_and_report_from_data(name, out_dir, profiles, ifts, raw_settings=Non
 
     for ift in ifts:
         raw.save_ift(ift, datadir=out_dir)
+
+
+
+"""
+Example use:
+import bioxtasraw.RAWAPI as raw
+
+pipeline_path = os.path.abspath(os.path.expanduser('~/Documents/software_dev/saxs-pipeline'))
+if pipeline_path not in os.sys.path:
+    os.sys.path.append(pipeline_path)
+
+from pipeline import do_analysis
+import pipeline.analysis
+
+#From files:
+do_analysis.analyze_and_report_from_processed_files('test_analysis.pdf', '.',
+    '../data', ['GI_sub_clean.dat'], [])
+
+
+#From data in memory:
+profiles = raw.load_profiles(['../data/GI_sub_clean.dat'])
+
+do_analysis.analyze_and_report_from_data('test_analysis_mem.pdf', '.', profiles,
+    [])
+
+"""
